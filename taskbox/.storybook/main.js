b29419289613c1b9a2f233ddf7e3da9b3adbe433
julbern <bernier13julie@gmail.com>
2022-01-07 15:50:23 -0500

    test commit

---
+++ taskbox/.storybook/main.js
@@ -0,0 +1,8 @@
+export const stories = ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"];
+export const staticDirs = ["../public"];
+export const addons = [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+];
