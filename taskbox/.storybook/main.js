1de84f18ca943b7bcf33db51432e10ea6b272d2a
julbern <bernier13julie@gmail.com>
2022-01-11 21:01:43 -0500

    modifier

--- taskbox/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-export const stories = ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"];
-export const staticDirs = ["../public"];
-export const addons = [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
-];
